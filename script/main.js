420d8f5231dfed22e9fc8c6339121f3b852036ba
m_zarandah <113542831+mohamedzrn@users.noreply.github.com>
2023-02-03 11:14:08 -0500

    added
    added changes from todays class

--- script/main.js
+++ script/main.js
@@ -1,30 +1,71 @@
-// select your elements first - what is the user going to interact
-// these are the targets=> these are what the "user" uses
-// this is a 1to 1 connection to an element in the DOM
-// let navButton = document.querySelector'#navButton');
+// select your elements first - what is the user going to interact with?
+// These are the targets => these are what "user" uses
+// this is a 1 to 1 connection to an element in the DOM
+// let navButton = document.querySelector('#navButton');
 
-// this is a 1 to many connection to elementss in the DOM
+// this is a 1 to many connection to an element in the DOM
 // the variable name is the "basket"
 let navButtons = document.querySelectorAll('#buttonHolder img'),
-	theHeadline = document.querySelector('#headLine h1'),
-	puzzleBoard = document.querySelector('.puzzle-board');
+    theHeadline = document.querySelector('#headLine h1'),
+	// collect draggable pieces
+	puzzlePieces = document.querySelectorAll('.puzzle-pieces img'),
+	dropZones = document.querySelectorAll('.drop-zone'),
+	puzzleBoard = document.querySelector('.puzzle-board'),
+	templink = document.querySelector('a'),
+	// set up a global variable to store a reference ti tge dragged piece
+	// i need ti jvoe this later when i drop it on a zone
+	draggedPiece;
 
-// functions go in the middle
-//these are the "actions" that should happen
-function changeBGImage() {
-	debugger;
-	// object.property = "new values"
-	// theHeadline.textContent = "Drag and Drop is fun!";
-	// theHeadline.classList.add('orange-headline');
 
-	// change the background image in the drop zone
-	puzzleBoard.style.backgroundImage = 'url("../images/backGround"' + this.id + '".jpg")';
-	puzzleBoard.style.backgroundImage: url("../../media/examples/lizard.png");
+// function go in the middle
+// these are the "actions" that should happen
+function changeBgImage() {
+	
+	// // object.property = "new value"
+	// // theHeadline.textContent = "Drag and Drop is funny";
+	// // theHeadline.classList.add("orange-headline");
+
+	// let newBGPath = "images/backGround" + this.id + ".jpg";
+	// // debugger;
+	// // change the background image in the drop zone
+	puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`;
 }
 
-// event handling at the bottom -> how things react when you
+function handleStartDrag() { 
+	// store the element I am currently dragging in that global draggedPiece variable
+	draggedPiece = this;
+}
+
+function handleDragOver(e) { e.preventDefault(); }
+
+function handleDrop(e) {
+	// block the default behaviour 
+	e.preventDefault();
+	// and then do whatever you want.
+	console.log('dropped on me!');
+	e.target.appendChild(draggedPiece);
+}
+
+
+// event handling at the bottom -> how things react when you use the targets
 // how is the user going to interact with the elements / controls you provide?
 
-//
+// 1 to 1 event handling (1 variable, one element):
+// navButton.addEventListener('click', changeBGImage);
+
+// 1 to many event handling (1 variable, many elements):
 // process a collection of elements and add an event handler to each
-navButtons.forEach(button => button.addEventListener('click', changeBGImage));
+navButtons.forEach(button => button.addEventListener('click', changeBgImage));
+// add the drag start handler to all the puzzle pieces
+puzzlePieces.forEach(piece => piece.addEventListener(`dragstart`, handleStartDrag));
+// add the dragover handling to the drop zones
+dropZones.forEach(zone => zone.addEventListener('dragover', handleDragOver));
+dropZones.forEach(zone => zone.addEventListener('drop', handleDrop));
+
+function blockfaultBehaviour(e) {
+	// dont let the default behaviour of certain elements happen - block it 
+	e.preventDefault();
+}
+
+// temp handling
+templink.addEventListener('click', blockfaultBehaviour)
